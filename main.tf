98ee7c9f84a97f2c5d50a8b6cdf9a7bb0c1ac011
Raul Herrera <raulch626@gmail.com>
2021-01-16 18:30:05 -0700

    Comment out key file.

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {  //Configures the named provider. For example, AWS.
 resource "aws_instance" "example" {  //Defines a piece of infrastructure. Also includes the resource type and resource name.
   ami           = var.amis[var.region]   //References the var.amis for dynamic lookup.
   instance_type = "t2.micro"
-  key_name = "A4L"
+  # key_name = "A4L"
   vpc_security_group_ids = [aws_security_group.allow_web.id]
   subnet_id = aws_subnet.sn-web-B.id
   user_data = file ("install_webserver.sh")
